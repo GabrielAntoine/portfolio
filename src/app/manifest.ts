0d3619e26c147b9724bb1354db7f19b8adc99119
GabrielAntoine <gabriel.antoine@student.hepl.be>
2026-04-26 00:19:31 +0200

    Update manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import { routing } from '@/i18n/routing'
+import { portfolioData } from '@/portfolio-data'
+import { MetadataRoute } from 'next'
+
+export default async function manifest(): Promise<MetadataRoute.Manifest> {
+  return {
+    name: portfolioData.name,
+    short_name: portfolioData.name,
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    theme_color: '#000000',
+    background_color: '#fc5c7d',
+    display: 'standalone',
+    lang: routing.defaultLocale,
+    start_url: '/',
+  }
+}
